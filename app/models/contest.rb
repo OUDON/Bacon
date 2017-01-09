4caab1241ff708d2ceccf06a5f7657fb0d2d0bc0
T.Tomita <ttp8101@gmail.com>
2017-01-10 00:01:00 +0900

    Make association

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,2 +1,6 @@
 class Contest < ApplicationRecord
+  has_many :contestants, dependent: :destroy
+  has_many :users,       through: :contestants
+
+  has_many :problems, dependent: :destroy
 end
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,2 +1,4 @@
 class Contestant < ApplicationRecord
+  belongs_to :contest
+  belongs_to :user
 end
