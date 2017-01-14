35e106182ac398283978494525f0617700f6314c
T.Tomita <ttp8101@gmail.com>
2017-01-14 22:34:40 +0900

    Use timezone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module Bacon
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
   end
 end
