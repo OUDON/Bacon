1daab420471c31b6d205b434933c72322927a7c9
T.Tomita <ttp8101@gmail.com>
2017-02-01 03:37:23 +0900

    Create contest

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -4,5 +4,19 @@ class ContestsController < ApplicationController
   end
 
   def create
+    @contest = Contest.new(contest_params)
+    if @contest.save
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @contest = Contest.find(params[:id])
+  end
+
+  private
+  def contest_params
+    params.require(:contest).permit(:title, :penalty_time, :start_at, :end_at)
   end
 end
