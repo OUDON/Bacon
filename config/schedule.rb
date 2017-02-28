3bd92b412721fad4f2253a45afd353d4601c14c4
T.Tomita <ttp8101@gmail.com>
2017-03-01 00:17:49 +0900

    Prevent overlapping cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,14 @@
 set :output, 'log/crontab.log'
-set :environment, :development
+env :PATH, ENV['PATH']
 
-job_type :runner, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rails runner :task :output"
+job_type :runner, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && bundle exec rails runner -e :environment :task :output"
+job_type :runner_with_setlock, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && setlock -n /tmp/:task bundle exec rails runner -e :environment :task :output"
 
 every :sunday, at: '0am' do
   runner 'DataUpdater.update_users_info'
 end
 
 every 2.minutes do
-  runner 'DataUpdater.update_submissions'
-  runner 'DataUpdater.update_standings'
+  runner_with_setlock 'DataUpdater.update_submissions'
+  runner_with_setlock 'DataUpdater.update_standings'
 end
