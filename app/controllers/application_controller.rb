cd633b5062b614378e040dd25fe1f948b0f230f3
T.Tomita <ttp8101@gmail.com>
2017-03-06 01:31:45 +0900

    Update user info at the registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :admin_user?
 
   def admin_user
@@ -19,11 +18,5 @@ class ApplicationController < ActionController::Base
 
   def admin_user?
     current_user && current_user.admin?
-  end
-
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :atcoder_id])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :atcoder_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'contests#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :contestants, only: [:create]
   resources :contests do
     resources :problems, only: [:create, :destroy]
   end
-
-  # post 'contests/:id/add_problem', to: 'contests#add_problem', as: :add_problem
 end
