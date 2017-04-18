1361443f431adb373a8fdc43909ea88313ede8a7
T.Tomita <ttp8101@gmail.com>
2017-04-18 19:15:51 +0900

    Add aoj_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308131222) do
+ActiveRecord::Schema.define(version: 20170418062417) do
 
   create_table "contestants", force: :cascade do |t|
     t.integer  "contest_id", null: false
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170308131222) do
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
     t.string   "name_color",             default: "#000000", null: false
+    t.string   "aoj_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
